1c727cb1e58efabdc0d6cf5755570cb7cda28fcc
nikhilanil2646 <nikhilanil2646@gmail.com>
2021-05-26 11:05:04 +0530

    FG

--- jobs/build_job.groovy
+++ jobs/build_job.groovy
@@ -3,16 +3,18 @@ freeStyleJob('jobs_folder/freestyle_child_job') {
         parameters{
 	   stringParam('JOB_NAME', '', 'Set the Gitlab repository URL')
 	   stringParam('GITLAB_REPOSITORY_URL', '', 'Set the Gitlab repository URL')
-			}
+	          }
 		 scm {
 	  git {
 	   remote {
 	   url 'https://github.com/nikhilanil2646/dsl-1' 
-	  }
+	         }
 	  branch 'master'
+              }
+
 		steps {
 			dsl {
 			external('jobs/job_commands.groovy')
 			    }
-			  }
-		 }
+		      }
+}
