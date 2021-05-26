4d5f04ecd7ce665ea7dcd39aa6599d007210a6a1
nikhilanil2646 <nikhilanil2646@gmail.com>
2021-05-26 12:02:31 +0530

    FG

--- jobs/build_job.groovy
+++ jobs/build_job.groovy
@@ -1,20 +1,22 @@
 folder("jobs_folder")
-freeStyleJob('jobs_folder/freestyle_child_job') {
+freeStyleJob('jobs_folder/freestyle_child_job') 
+{
         parameters{
 	   stringParam('user_name', '', 'Enter the username')
 	          }
-		 scm {
-	  git {
-	   remote {
-	   url 'https://github.com/nikhilanil2646/dsl-1' 
-	         }
-	  branch 'master'
-              }
 
-		    }
-		steps {
-			dsl {
+	scm {
+	  	git {
+	   	    remote {
+	                   url 'https://github.com/nikhilanil2646/dsl-1' 
+	                   }
+	            branch 'master'
+                    }
+	    }
+
+	steps {
+		dsl {
 			external('jobs/job_commands.groovy')
-			    }
-		      }
+		    }
+	      }
 }
--- jobs/job_commands.groovy
+++ jobs/job_commands.groovy
@@ -1,1 +1,2 @@
 println "child job is successfully runned by child_job by user ${user_name}"
+echo "hello, ${user_name}" 
