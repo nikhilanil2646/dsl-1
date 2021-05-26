b31bf4997c273c0a399d369c4ee87268abc523a4
nikhilanil2646 <nikhilanil2646@gmail.com>
2021-05-26 14:22:29 +0530

    FG

--- jobs/build_job.groovy
+++ jobs/build_job.groovy
@@ -2,7 +2,7 @@ folder("jobs_folder")
 freeStyleJob('jobs_folder/freestyle_child_job') 
 {
         parameters{
-	   stringParam(name:'user_name', default:"nikhil")
+	   stringParam(name:'user_name', defaultvalue:"nikhil")
 	          }
 
 	scm {
