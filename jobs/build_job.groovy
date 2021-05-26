94200542d8208547e183f25baed71cf05d370feb
nikhilanil2646 <nikhilanil2646@gmail.com>
2021-05-26 14:31:07 +0530

    FG

--- jobs/build_job.groovy
+++ jobs/build_job.groovy
@@ -18,8 +18,7 @@ freeStyleJob('jobs_folder/freestyle_child_job')
 		dsl {
 			external('jobs/job_commands.groovy')
 		    }
+		build 'jobs_folder/freestyle_child_job'
 	      }
-	publishers {
-	  downstream 'jobs_folder/freestyle_child_job' , 'SUCCESS'
-		   }
+	
 }
